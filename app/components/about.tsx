6799cd5fd80deca494afb2d2b20077492c6e32e3
Rodrigo Leon <yoyiyomeco@hotmail.com>
2024-06-03 23:30:25 -0700

    new about me and github added in footer

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,30 +1,29 @@
-import ProfileCheck from "./img/profile";
-
-const ProfileSection = () => (
-  <div className="profile-section flex-col justify-center py-11">
-    <div className="flex items-center p-5">
-      <ProfileCheck />
-      <h1 className="profile-title text-2xl pl-3 text-white">Sobre Mí</h1>
-    </div>
-  </div>
-);
-
-const AboutText = () => (
-  <div className="flex">
-    <p className="flex-grow">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi earum
-      adipisci nobis, odio et quidem ex accusamus facilis beatae temporibus
-      nostrum incidunt ratione error. Consequuntur, similique. Atque itaque
-      animi a.
-    </p>
-  </div>
-);
+import ProfileCheck from "../assets/me.png";
 
 export default function About() {
   return (
     <article id="about" className="about-section w-full mx-auto lg:w-1/2 pb-24">
-      <ProfileSection />
-      <AboutText />
+      <div className="profile-section flex-col justify-center py-11">
+        <div className="flex items-center gap-4 p-5">
+          <span className=
+            "bg-gray-800 size-[165px] rounded-full transform translate-x-1 translate-y-1">
+          </span>
+          <img
+            src={ProfileCheck}
+            alt="Rodrigo Leon"
+            className="absolute size-[165px] rounded-full"
+            draggable="false"
+          />  
+          <h1 className=" text-2xl pl-3 text-white">Sobre Mí</h1>
+        </div>
+      </div>
+      <div className="flex">
+        <p className="flex-grow text-[23px]">
+          Soy una persona apasionada por dar vida a las ideas.
+          Me encanta aprender y experimentar con nuevas tecnologías.
+          Me considero autodidacta y disfruto aprender cosas nuevas para aplicarlas en mis proyectos.
+        </p>
+      </div>
     </article>
   );
 }
--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import Github from "./img/Github";
+import { Link } from "@remix-run/react";
 
 export default function Footer() {
   const [year, setYear] = useState(new Date().getFullYear());
@@ -6,10 +8,18 @@ export default function Footer() {
     setYear(new Date().getFullYear());
   }, []);
   return (
-    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
+    <footer className="flex flex-col justify-between gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
       <p className="text-xs text-gray-400">
         &copy; {year} Rodrigo Gibran Felix Leon. Todos los derechos reservados
       </p>
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        to="https://github.com/Yoyiyoniu"
+        className="flex items-center gap-5 hover:outline">
+        Mira en que estoy trabajando!
+        <Github />
+      </Link>
     </footer>
   );
 }
